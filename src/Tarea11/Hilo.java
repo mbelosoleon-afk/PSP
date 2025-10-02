75fd2fe2609e3f3fd7b957bd1c3ac17d65804b2e
dam <mbelosoleon@danielcastelao.org>
2025-10-02 10:26:42 +0200

    segunda opcion

---
+++ src/Tarea11/Hilo.java
@@ -0,0 +1,59 @@
+package Tarea11;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hilo extends Thread{
+    private int numero;
+    private String nombre;
+    private int limite;
+
+    public Hilo(String nombre, int numero, int limite) {
+            this.nombre = nombre;
+            this.numero = numero;
+            this.limite = limite;
+        }
+
+        @Override
+        public void run() {
+            Random random = new Random();
+            if(numero < limite){
+                Hilo siguienteHilo = new Hilo ("Hilo " + (numero + 1), numero + 1, limite);
+                siguienteHilo.start();
+                for(int i=1; i<5;i++){
+                    System.out.println("Iteración " +i+ " del " + nombre);
+                    try {
+                        int tiempo = random.nextInt(501) + 100;
+                        Thread.sleep(tiempo + 1000);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                if(siguienteHilo != null) {
+                    try{
+                        siguienteHilo.join();
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Acabó hilo " + nombre);
+            }
+        }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int nHilos;
+            System.out.println("Elija el número de hilos");
+            if(scan.hasNext()){
+                nHilos = scan.nextInt();
+            }else{
+                System.out.println("No ha introducido número, se le asignará un 5 al valor");
+                nHilos = 5;
+            }
+
+        if(nHilos >= 1){
+            new Hilo("Hilo 1",1,nHilos).start();
+        }
+        scan.close();
+    }
+}
+
