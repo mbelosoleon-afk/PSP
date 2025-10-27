02a002e587602581b4d19bbed93c7a2b70e10793
dam <mbelosoleon@danielcastelao.org>
2025-10-27 13:26:33 +0100

    Repaso2 fin

--- src/Repaso/repaso2/Main.java
+++ src/Repaso/repaso2/Main.java
@@ -1,75 +1,62 @@
 package repaso2;
 
-import javax.print.attribute.standard.Copies;
 
 public class Main {
-private static final Object miLockPieza = new Object();
-private static final Object miLockPintura = new Object();
-private static final Object miLockEmbalaje = new Object();
-
-        static class T {
-            void Tarea1(){
-                synchronized (miLockPieza){
-                    Pieza p = new Pieza();
-                    p.CogerPieza();
-                    try{
-                        Thread.sleep(2000);
-                    }catch (InterruptedException e){
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-            void Tarea2(){
-                synchronized (miLockPintura){
-                    Pieza p = new Pieza();
-                    p.PintarPieza();
-                    try{
-                        Thread.sleep(1000);
-                    }catch (InterruptedException e){
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-            void Tarea3(){
-                synchronized (miLockEmbalaje){
-                    Pieza p = new Pieza();
-                    p.EmbalandoPieza();
-                    try{
-                        Thread.sleep(1000);
-                    }catch (InterruptedException e){
-                        throw new RuntimeException(e);
-                    }
+    private static final Object miLockPieza =new Object();
+    private static final Object miLockPintura =new Object();
+    private static final Object miLockEmbalaje =new Object();
+    static class T {
+        void Tarea1() {
+            synchronized (miLockPieza) {
+                Pieza p = new Pieza();
+                p.CogerPieza();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         }
-    static T t = new T();
 
-        static Thread operario1 = new Thread(()->t.Tarea1(),"operario 1");
-        static Thread operario2 = new Thread(()->t.Tarea2(),"operario 2");
-        static Thread operario3 = new Thread(()->t.Tarea3(),"operario 3");
-
-        public static void main(String[] args){
-            System.out.println(operario1.getName());
-            operario1.start();
-            try{
-                operario1.join();
-            }catch (InterruptedException e){
-                throw new RuntimeException(e);
+        void Tarea2() {
+            synchronized (miLockPintura) {
+                Pieza p = new Pieza();
+                p.PintarPieza();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
-            System.out.println(operario2.getName());
-            operario2.start();
-            try{
-                operario2.join();
-            }catch (InterruptedException e){
-                throw new RuntimeException(e);
-            }
-            System.out.println(operario3.getName());
-            operario3.start();
-            try{
-                operario3.join();
-            }catch (InterruptedException e){
-                throw new RuntimeException(e);
-            }
-            System.out.println("Pieza lista para lanzar");
         }
+
+        void Tarea3() {
+            synchronized (miLockEmbalaje) {
+                Pieza p = new Pieza();
+                p.EmbalandoPieza();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+    static T t=new T();
+
+    static Thread h1=new Thread(()->t.Tarea1(),"operador 1");
+    static Thread h2=new Thread(()->t.Tarea2(),"operador 2");
+    static Thread h3=new Thread(()->t.Tarea3(),"operador 3");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(h1.getName());
+        h1.start();
+        h1.join();
+        System.out.println(h2.getName());
+        h2.start();
+        h2.join();
+        System.out.println(h3.getName());
+        h3.start();
+        h3.join();
+        System.out.println("Pieza lista para lanzar");
+    }
 }
