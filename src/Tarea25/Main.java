df75fa82877a5cdd63e2e0f3901e5613eaf942cc
dam <mbelosoleon@danielcastelao.org>
2025-11-04 10:20:17 +0100

    Falta case 5 y 6

--- src/Tarea25/Main.java
+++ src/Tarea25/Main.java
@@ -1,13 +1,40 @@
 package Tarea25;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String direccionn = scan.nextLine();
-
-
+        int opcion;
+        String ip;
+        ArrayList<String> resumen = new ArrayList<>();
+        while(true){
+            System.out.println("Indica la IP");
+            ip = scan.next();
+            System.out.println("Elige un puerto: 1:21, 2:22, 3:80, 4:443, 5:Otro, 6:Salir");
+            opcion = scan.nextInt();
+            switch (opcion){
+                case 1 -> opcion = 21;
+                case 2 -> opcion = 22;
+                case 3 -> opcion = 80;
+                case 4 -> opcion = 443;
+                //case 5 y 6
+            }
+            if(opcion<0) break;
+            try{
+                Socket socket = new Socket(ip,opcion);
+                resumen.add("IP: " + ip + " Puerto: " + opcion + " Abierto");
+                socket.close();
+            }catch (UnknownHostException e){
+                System.out.println("Host desconocido: " + e.getLocalizedMessage());
+            }catch (IOException e){
+                resumen.add("IP: "+ip+" Puerto: "+opcion+" Cerrado");
+            }
+            System.out.println(resumen);
+        }
     }
 }
