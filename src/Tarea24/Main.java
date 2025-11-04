1c03fc0432816fd9d751210235720458ed4f1829
dam <mbelosoleon@danielcastelao.org>
2025-11-04 09:52:34 +0100

    24 hecha

--- src/Tarea24/Main.java
+++ src/Tarea24/Main.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        String direccion = scan.nextLine();
         System.out.println("Escriba la dirección");
+        String direccion = scan.nextLine();
         try{
             InetAddress ip = InetAddress.getByName(direccion);
             System.out.println(ip.getHostName() + " - " + ip.getHostAddress());
--- src/Tarea25/Main.java
+++ src/Tarea25/Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String direccionn = scan.nextLine();
+
 
     }
 }
