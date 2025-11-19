1826c5362a3952ac6c322cf3b2669fac5b68c49b
dam <mbelosoleon@danielcastelao.org>
2025-11-19 11:02:07 +0100

    Tarea32 Hecha

---
+++ src/Tarea32/Cliente.java
@@ -0,0 +1,47 @@
+package Tarea32;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+
+        int puerto_servidor = 6666;
+        byte[] buffer = new byte[1024];
+        String[] palabras = {"Palabra","PalabraLarga","PalabraMasLarga"};
+
+
+        try {
+            InetAddress direccionServidor = InetAddress.getByName("localhost");
+
+            DatagramSocket datagramSocket = new DatagramSocket();
+            DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+
+
+            String msj = Arrays.toString(palabras);
+            buffer = msj.getBytes();
+
+            DatagramPacket pregunta = new DatagramPacket(buffer, buffer.length, direccionServidor, puerto_servidor);
+            datagramSocket.send(pregunta);
+            System.out.println("Enviado el mensaje al servidor");
+
+            datagramSocket.receive(peticion);
+
+            System.out.println("Recibo msj del servidor");
+
+            String msjServidor = new String(peticion.getData(),0, peticion.getLength());
+            System.out.println("Respuesta del servidor = " + msjServidor);
+
+        } catch (SocketException ex) {
+            System.out.println("Error: "+ ex.getMessage());
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/Tarea32/Servidor.java
@@ -0,0 +1,49 @@
+package Tarea32;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class Servidor {
+
+    public static void main(String[] args) {
+        int puerto = 6666;
+        byte[] buffer = new byte[1024];
+
+        try (DatagramSocket datagramSocket = new DatagramSocket(puerto)){
+            System.out.println("Servidor arrancando");
+
+            DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+
+            datagramSocket.receive(peticion);
+            System.out.println("Recibida peticion");
+
+            String msj = new String(peticion.getData(), 0, peticion.getLength());
+            System.out.println("listado de palabras = " + msj);
+
+            msj=msj.replace("[","");
+            msj=msj.replace("]","");
+            String[] palabras = msj.split(",");
+            String palabraLarga = palabras[0];
+            for (String palabra:palabras){
+                if (palabraLarga.length()<palabra.length()) palabraLarga = palabra;
+            }
+
+            int puertoCliente = peticion.getPort();
+            InetAddress direccion = peticion.getAddress();
+
+            String msjServidor = palabraLarga;
+            buffer = msjServidor.getBytes();
+            DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length, direccion, puertoCliente);
+            datagramSocket.send(respuesta);
+            System.out.println("Envié respuesta al cliente");
+
+        } catch (SocketException ex) {
+            System.out.println("Error con el socket(servidor): "+ ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("Error IO(servidor): "+ ex.getMessage());
+        }
+    }
+}
